a7e3a2a6996d82652a5c8f775830456d83363019
Sardor Allaberganov <sardorallaberganov96@gmail.com>
2020-05-21 17:39:47 +0500

    last change

--- data/js/index.js
+++ data/js/index.js
@@ -1,6 +1,20 @@
-$(document).ready(function(){
-    $(".read-btn").click(function(){
+$(document).ready(function () {
+    $(".read-btn").click(function () {
         $(this).children().toggleClass("chevron-style");
     });
-    $('.selectpicker').selectpicker({title: 'Навыки'});
+    $('.selectpicker').selectpicker({ title: 'Навыки' });
+
+    $(".show-password").click(function () {
+        var input = $(this).siblings();
+        if (input.attr("type") == 'password') {
+            input.attr("type", "text");
+            $(this).children().attr("src", "data/img/eye-slash.svg");
+        }
+        else {
+            input.attr("type", "password");
+            $(this).children().attr("src", "data/img/eye.svg");
+        }
+    });
+
+    
 });
