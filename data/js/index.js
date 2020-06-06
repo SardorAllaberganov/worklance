19d98b42192c9486817bcd915a4fa0f925a2e3ee
Sardor Allaberganov <sardorallaberganov96@gmail.com>
2020-06-06 19:29:08 +0500

    last

--- data/js/index.js
+++ data/js/index.js
@@ -15,5 +15,25 @@ $(document).ready(function () {
             $(this).children().attr("src", "data/img/eye.svg");
         }
     });
+
+    $(".input-style").keyup(function () {
+        $(this).siblings().find("button:not(.btn)").removeClass("back-btn");
+        $(this).siblings().find("button:not(.btn)").addClass("active-btn");
+        console.log("typing");
+        if ($(this).val().length === 0) {
+            $(this).siblings().find("button:not(.btn)").addClass("back-btn");
+            $(this).siblings().find("button:not(.btn)").removeClass("active-btn");
+        }
+    });
+
+    $('.modal-delete-btn').prop('disabled', true);
+    $('#delete-input').keyup(function () {
+        if ($(this).val() != '') {
+            $('.modal-delete-btn').prop('disabled', false);
+        }
+        else{
+            $('.modal-delete-btn').prop('disabled', true);
+        }
+    });
 });
 
